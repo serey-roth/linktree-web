173d16e8ce9e3edf87c9a406cecf868e7d34f047
Serey Roth <sereyratanakroth@gmail.com>
2023-02-25 20:13:17 -0800

    Move api configuration to openApi.ts

--- src/utils/hooks/useLogin.tsx
+++ src/utils/hooks/useLogin.tsx
@@ -1,11 +1,6 @@
-import { Configuration, UsersApi, LoginInput } from "@/generated/openapi";
+import { LoginInput } from "@/generated/openapi";
 import { useMutation } from "react-query";
-
-const configuration = new Configuration({
-    basePath: 'http://localhost:8080/api'
-});
-
-const usersApi = new UsersApi(configuration);
+import { usersApi } from "../openApi";
 
 export const useLogin = () => {
     const { mutateAsync, ...rest } = useMutation(
