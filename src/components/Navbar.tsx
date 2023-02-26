c0da679c7b22c8f6f9a7fc8e0e56bd99f65d3955
Serey Roth <sereyratanakroth@gmail.com>
2023-02-25 20:16:08 -0800

    Update Navbar to display current user if existed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Link from 'next/link';
+import { useMe } from '@/utils/hooks/useMe';
 
 interface NavbarProps {
 
 }
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
+    const { data } = useMe();
+
     return (
         <div className='w-full flex items-center justify-between
         py-2'>
@@ -13,18 +16,35 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
                 Linktree Clone
             </h1>
             <div className='flex items-center gap-2'>
-                <Link 
-                className='hover:underline 
-                transition duration:100 ease-in-out'
-                href='/auth/login'>
-                    login
-                </Link>
-                <Link 
-                className='hover:underline
-                transition duration:100 ease-in-out'
-                href='/auth/register'>
-                    register
-                </Link>
+                {data?.user ? (
+                    <>  
+                        <div className='rounded-full py-1 px-3
+                        flex items-center justify-center 
+                        bg-teal-400'>
+                            <p>
+                            {data.user.username.charAt(0)}
+                            </p>
+                        </div>
+                        <Link href='/auth/login'>
+                            log out
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                        <Link 
+                        className='hover:underline 
+                        transition duration:100 ease-in-out'
+                        href='/auth/login'>
+                            log in
+                        </Link>
+                        <Link 
+                        className='hover:underline
+                        transition duration:100 ease-in-out'
+                        href='/auth/register'>
+                            register
+                        </Link>
+                    </>
+                )}
             </div>
         </div>
     );
