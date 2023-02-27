92d258f05f26176966614433f04d53fcbe613760
Serey Roth <sereyratanakroth@gmail.com>
2023-02-26 17:43:46 -0800

    Display validation errors in Login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,5 +1,5 @@
 import { Layout } from '@/components/Layout';
-import { UsernameAndPassword } from '@/generated/openapi';
+import { ModelError, MultipleErrors, UsernameAndPassword } from '@/generated/openapi';
 import { useLogin } from '@/utils/hooks/useLogin';
 import { useRouter } from 'next/router';
 import React from 'react';
@@ -9,11 +9,20 @@ interface LoginProps {
 
 }
 
+const isMultipleErrors = (error: ModelError): error is MultipleErrors => {
+    return (error as MultipleErrors).errors !== undefined;
+}
+
 const Login: React.FC<LoginProps> = ({}) => {
     const router = useRouter();
     const { username: name } = router.query;
 
-    const { register, handleSubmit } = useForm<UsernameAndPassword>({
+    const { 
+        formState: { errors },
+        register, 
+        handleSubmit, 
+        setError
+    } = useForm<UsernameAndPassword>({
         values: {
             username: name && typeof name === 'string' ? name : '',
             password: ''
@@ -23,11 +32,17 @@ const Login: React.FC<LoginProps> = ({}) => {
     const { login } = useLogin();
 
     const onSubmit: SubmitHandler<UsernameAndPassword> = async (values) => {
-        if (values.username && values.password) {
-            const result = await login(values);
-            if (result.data) {
-                router.push('/');
-            }
+        const result = await login(values);
+        if (result.data) {
+          router.push("/");
+        }
+        if (result.error && isMultipleErrors(result.error)) {
+          result.error.errors.forEach(({ field, message }) => {
+            setError(field as keyof UsernameAndPassword, {
+              type: "custom",
+              message,
+            });
+          });
         }
     }
 
@@ -37,24 +52,36 @@ const Login: React.FC<LoginProps> = ({}) => {
             justify-center items-center mt-4'>
                 <form 
                 className=' max-w-[400px] w-full flex flex-col
-                gap-2'
+                gap-3'
                 onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                    className='p-1 rounded-sm bg-gray-400/20
-                    ' 
-                    id='username'
-                    type='text'
-                    placeholder='Username'
-                    {...register('username')}
-                    />
-                    <input
-                    className='p-1 rounded-sm bg-gray-400/20
-                    ' 
-                    id='password'
-                    type='password'
-                    placeholder='Password'
-                    {...register('password')}
-                    />
+                    <div className='flex flex-col gap-1'>
+                        <input
+                        className='p-1 rounded-sm bg-gray-400/20' 
+                        id='username'
+                        type='text'
+                        placeholder='Username'
+                        {...register('username')}
+                        />
+                        {errors.username && (
+                            <p className='text-red-500 text-sm'>
+                                {errors.username.message}
+                            </p>
+                        )}  
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <input
+                        className='p-1 rounded-sm bg-gray-400/20' 
+                        id='password'
+                        type='password'
+                        placeholder='Password'
+                        {...register('password')}
+                        />
+                        {errors.password && (
+                            <p className='text-red-500 text-sm'>
+                                {errors.password.message}
+                            </p>
+                        )}
+                    </div>
                     <button 
                     className='bg-blue-500/20 
                     p-1 rounded-sm'
