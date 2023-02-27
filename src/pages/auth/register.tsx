7dbc598e84b27e7548b4f6e7bfd3185c24ffb986
Serey Roth <sereyratanakroth@gmail.com>
2023-02-26 18:24:25 -0800

    Set up error displays and use InputField in Register

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,9 +1,11 @@
+import { InputField } from '@/components/InputField';
 import { Layout } from '@/components/Layout';
 import { UsernameEmailAndPassword, ModelApiResponse } from '@/generated/openapi';
 import { useRegister } from '@/utils/hooks/useRegister';
+import { isMultipleErrors } from '@/utils/isMultipleErrors';
 import { useRouter } from 'next/router';
 import React from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 
 interface RegisterProps {
 
@@ -11,16 +13,22 @@ interface RegisterProps {
 
 const Register: React.FC<RegisterProps> = ({}) => {
     const router = useRouter();
-    const { register, handleSubmit } = useForm<UsernameEmailAndPassword>();
+    const methods = useForm<UsernameEmailAndPassword>();
 
     const { register: signUp } = useRegister();
 
     const onSubmit: SubmitHandler<UsernameEmailAndPassword> = async (values) => {
-        if (values.username && values.password && values.email) {
-            const result = await signUp({...values});
-            if (result.data) {
-                router.push(`/auth/login/?username=${values.username}`);
-            }
+        const result = await signUp({ ...values });
+        if (result.data) {
+            router.push(`/auth/login/?username=${values.username}`);
+        }
+        if (result.error && isMultipleErrors(result.error)) {
+            result.error.errors.forEach(({ field, message }) => {
+                methods.setError(field as keyof UsernameEmailAndPassword, {
+                    type: "custom",
+                    message,
+                });
+            });
         }
     }
 
@@ -28,38 +36,37 @@ const Register: React.FC<RegisterProps> = ({}) => {
         <Layout>
             <div className='flex flex-col w-full h-full
             justify-center items-center mt-4'>
-                <form 
-                className=' max-w-[400px] w-full flex flex-col
-                gap-2'
-                onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                    className='p-1 rounded-sm bg-gray-400/20' 
-                    id='username'
-                    type='text'
-                    placeholder='Username'
-                    {...register('username')}
-                    />
-                    <input
-                    className='p-1 rounded-sm bg-gray-400/20' 
-                    id='email'
-                    type='text'
-                    placeholder='Email'
-                    {...register('email')}
-                    />
-                    <input
-                    className='p-1 rounded-sm bg-gray-400/20' 
-                    id='password'
-                    type='password'
-                    placeholder='Password'
-                    {...register('password')}
-                    />
-                    <button 
-                    className='bg-blue-500/20 
-                    p-1 rounded-sm'
-                    type='submit'>
-                        Log in
-                    </button>
-                </form>
+                <FormProvider {...methods}>
+                    <form 
+                    className=' max-w-[400px] w-full flex flex-col
+                    gap-3'
+                    onSubmit={methods.handleSubmit(onSubmit)}>
+                        <InputField 
+                        id='username'
+                        name='username'
+                        type='text'
+                        placeholder='Username'/>
+
+                        <InputField 
+                        id='email'
+                        name='email'
+                        type='text'
+                        placeholder='Email'/>
+
+                        <InputField 
+                        id='password'
+                        name='password'
+                        type='password'
+                        placeholder='Password'/>
+
+                        <button 
+                        className='bg-blue-500/20 
+                        p-1 rounded-sm'
+                        type='submit'>
+                            Register
+                        </button>
+                    </form>
+                </FormProvider>
             </div>
         </Layout>
     );
