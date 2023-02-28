254ee68927f0f12e1088d81ad37ffbe7b6ea0dfd
Serey Roth <sereyratanakroth@gmail.com>
2023-02-27 20:58:11 -0800

    Update useSortedPaginatedLinks query function

--- src/utils/hooks/useSortedPaginatedLinks.ts
+++ src/utils/hooks/useSortedPaginatedLinks.ts
@@ -1,30 +1,19 @@
 import { SecureLinksPaginatedSortedGetRequest } from "@/generated/openapi";
-import { QueryFunctionContext, QueryKey, useQuery } from "react-query";
+import { useQuery } from "react-query";
 import { linksApi } from "../openApi";
-
-const PAGE_COUNT = 5;
-const PAGE_NUMBER = 1;
 
 type SortedPaginatedPayload = SecureLinksPaginatedSortedGetRequest;
 
-const getSortedPaginatedLinks = async (key: QueryFunctionContext<QueryKey, 
-    SortedPaginatedPayload>) => {
-    return linksApi.withPreMiddleware().secureLinksPaginatedSortedGet({
-        pageCount: key.pageParam?.pageCount || PAGE_COUNT ,
-        pageNumber: key.pageParam?.pageNumber || PAGE_NUMBER,
-        sortKey: key.pageParam ? key.pageParam.sortKey : "createdAt",
-        order: key.pageParam?.order || "DESC"
-        
-    });
+const getSortedPaginatedLinks = async (payload: SortedPaginatedPayload) => {
+    return linksApi.withPreMiddleware().secureLinksPaginatedSortedGet(payload);
 }
 
 export const useSortedPaginatedLinks = (payload: SortedPaginatedPayload) => {
     const query = useQuery(
         ['sorted-paginated-links', payload], 
-        getSortedPaginatedLinks,
+        () => getSortedPaginatedLinks(payload),
         {
             keepPreviousData: true,
-            enabled: false,
         }
     )
 
