10d1f593dda0a18eb0a643f665376cb0a50eb442
Serey Roth <sereyratanakroth@gmail.com>
2023-02-28 22:55:15 -0800

    Use useAddLink hook inside AddLink component

--- src/components/AddLink.tsx
+++ src/components/AddLink.tsx
@@ -1,27 +1,24 @@
-import { Link } from '@/utils/types/Link';
-import React from 'react'
+import { LinkPayload } from '@/generated/openapi';
+import { useAddLink } from '@/utils/hooks/useAddLink';
+import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
-type LinkInput = {
-    title: string;
-    url: string;
-}
-
 interface AddLinkProps {
-    onNewLink: (link: Link) => void;
 }
 
-export const AddLink: React.FC<AddLinkProps> = ({
-    onNewLink
-}) => {
-    const { register, handleSubmit } = useForm<LinkInput>();
+export const AddLink: React.FC<AddLinkProps> = ({}) => {
+    const { addLink } = useAddLink();
 
-    const onSubmit: SubmitHandler<LinkInput> = ({ title, url }) => {
-        onNewLink({ title, href: url });
+    const { register, handleSubmit } = useForm<LinkPayload>();
+    
+    const onSubmit: SubmitHandler<LinkPayload> = async (values) => {
+        addLink(values);
     }
 
     return (
-        <form 
+        <form
+        method='post'
+        action='http://localhost:8080/api/secure/links'
         className='flex flex-col w-full gap-1 mt-2'
         onSubmit={handleSubmit(onSubmit)}>
             <input 
@@ -38,6 +35,13 @@ export const AddLink: React.FC<AddLinkProps> = ({
                 type='url'
                 placeholder='Url'
                 {...register('url')}
+            />
+            <input 
+                className='p-2 border rounded-lg
+                shadow-inner shadow-slate-300'
+                type='text'
+                placeholder='Description'
+                {...register('description')}
             />
             <button 
                 className='bg-teal-400 p-2 rounded-lg
