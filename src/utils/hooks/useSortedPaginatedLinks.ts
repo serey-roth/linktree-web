651b7136b3fd40aec6476b1e2832af423f25f86b
Serey Roth <sereyratanakroth@gmail.com>
2023-02-28 19:40:59 -0800

    Disable automatic query for useSortedPaginatedLinks

--- src/utils/hooks/useSortedPaginatedLinks.ts
+++ src/utils/hooks/useSortedPaginatedLinks.ts
@@ -14,6 +14,7 @@ export const useSortedPaginatedLinks = (payload: SortedPaginatedPayload) => {
         () => getSortedPaginatedLinks(payload),
         {
             keepPreviousData: true,
+            enabled: false
         }
     )
 
