2365d2f55153cad24bb29af89a68811fe18890bb
Serey Roth <sereyratanakroth@gmail.com>
2023-03-14 16:26:41 -0700

    Implement Compound pattern in LinkList

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -1,41 +1,24 @@
-import { Link } from "@/generated/openapi";
-import { useDeleteLink } from "@/utils/hooks/useDeleteLink";
-import React from "react";
-import { LinkCard } from "./LinkCard";
-
-type IndexedLink = Link & {
-    [key: string]: any;
-}
+import React, { ReactNode } from "react";
 
 interface LinkListProps {
-    links?: Link[];
+    children: ReactNode;
     isFetching: boolean;
+    length: number;
 }
 
 export const LinkList: React.FC<LinkListProps> = ({ 
-    links,
+    children,
     isFetching,
+    length
 }) => {
-    const { deleteLink } = useDeleteLink();
-
-    const handleDeleteLink = (index: number) => {
-        deleteLink(index);
-    }
-
     return (
         <div className="flex flex-col mt-2 w-full gap-2 max-h-[500px]
         overflow-y-auto">
             {isFetching && <p className='text-slate-300 text-center'>
                 Loading...
             </p>}
-            {links?.map((link) => (
-                <LinkCard 
-                key={link.id}
-                link={link as IndexedLink}
-                onDelete={() => handleDeleteLink(link.id)}
-                />
-            ))}
-            {!isFetching && links?.length === 0 && (
+            {children}
+            {!isFetching && length === 0 && (
                 <p className='text-slate-300 text-center'>
                     No links
                 </p>
