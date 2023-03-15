9adc09e82068f344006896664da5acc457a754af
Serey Roth <sereyratanakroth@gmail.com>
2023-03-14 18:46:51 -0700

    Clear me data in cache after successful log out

--- src/utils/hooks/useLogout.ts
+++ src/utils/hooks/useLogout.ts
@@ -1,9 +1,32 @@
-import { useMutation } from "react-query";
+import { AuthResponse } from "@/generated/openapi";
+import { useMutation, useQueryClient } from "react-query";
+import { Updater } from "react-query/types/core/utils";
 import { usersApi } from "../openApi";
 
 export const useLogout = () => {
+    const queryClient = useQueryClient();
+
+    const updater: Updater<AuthResponse | undefined, AuthResponse | undefined> = (meData: AuthResponse | undefined) => {
+        if (meData) {
+            return {
+                ...meData,
+                data: null,
+            }
+        }
+        return meData;
+    };
+
     const { mutateAsync, ...rest } = useMutation(
-        () => usersApi.authLogoutPost()
+        () => usersApi.authLogoutPost(), {
+            onSuccess: () => {
+                //don't use queryClient.invalidateQueries
+                //since we disable automatic fetching for useMe hook
+                queryClient.setQueryData<AuthResponse | undefined>(
+                    ['me'],
+                    updater
+                )
+            }
+        }
     );
 
     const logout = () => {
